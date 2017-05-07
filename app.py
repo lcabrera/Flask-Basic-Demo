9d48cd044798c31833ae30ed0b8be236ba953248
Desarrollos PICSL (Casa) <desarrollo@pic-sl.com>
2017-05-07 14:57:26 +0100

    Aplicación inicial

---
+++ app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Initial app."""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Índice del proyecto."""
+    return '<h1>Hello World!</h1>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
