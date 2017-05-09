010a1113a91709e57f3a881b1972a2330fefd49b
Desarrollos PICSL (Casa) <desarrollo@pic-sl.com>
2017-05-09 23:49:13 +0100

    Plantillas, documentación y test

---
+++ test/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""FBD.
+
+__init__.py is a special Python file that allows a directory to become
+a Python package so it can be accessed using the 'import' statement.
+"""
+
+__author__ = 'Luis Cabrera Sauco'
+__email__ = 'icorrecam@gmail.com'
+# __version__ = '0.0.0'
+
+# END
